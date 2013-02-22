994b06bf31e673c9bf6cc10655cbeddcc6bae998
Diogo Nunes <dialex.nunes@gmail.com>
2013-02-22 18:35:51 +0000

    create branch for "voting" feature
    (I also did some clean up, just to have something to commit)

--- parliament/views.py
+++ parliament/views.py
@@ -1,7 +1,6 @@
 from django.template import RequestContext
-from django.shortcuts import get_object_or_404, render, render_to_response
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
 from parliament.models import Proposal, Tag, CreateProposalForm
 
 def proposals(request):
@@ -30,23 +29,6 @@ def proposal_create(request):
 				'form': form,
 			})
 
-def proposal_create_finish(request):
-	# try:
-		# title = request.POST['title']
-		# desc = request.POST['desc']
-	# except (KeyError):
-		# # Redisplay the poll voting form.
-		# return render_to_response('parliament/proposal_create.html', {
-			# 'error_message': "Please fill out all fields.",
-			# }, context_instance=RequestContext(request))
-	# new_prop = Proposal.objects().insert(title, desc)
-	# Proposal.objects().save
-	# Always return an HttpResponseRedirect after successfully dealing
-	# with POST data. This prevents data from being posted twice if a
-	# user hits the Back button.
-	#return HttpResponseRedirect(reverse('parliament.views.proposal_detail', args=1
-	return
-	
 def tags(request):
 	tags_list = Tag.objects.all().order_by('name')
 	return render(request, 'parliament/tags.html', {'tags_list': tags_list})
@@ -58,5 +40,5 @@ def tag_detail(request, tag_name):
 	return render(request, 'parliament/tag_detail.html', {'tag': tag, 'tagged_proposals': tagged_proposals})
 
 def index(request):
-    proposals_list = Proposal.objects.all().order_by('-timestamp')
-    return render(request, 'parliament/index.html', {'proposals_list': proposals_list})
+	proposals_list = Proposal.objects.all().order_by('-timestamp')
+	return render(request, 'parliament/index.html', {'proposals_list': proposals_list})
