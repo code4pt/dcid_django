9bcf805befacaf4b06d5719b3f03de6515e56e4c
Diogo Nunes <dialex.nunes@gmail.com>
2013-06-20 18:17:37 +0100

    setup base for authentication

--- parliament/urls.py
+++ parliament/urls.py
@@ -2,13 +2,22 @@ from django.conf.urls import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('parliament.views', #prefix
+	# Home
 	url(r'^$', 'index'),
+	
+	# Tags
 	url(r'^tags/$', 'tags'),
 	url(r'^tags/(?P<tag_name>\S+)/$', 'tag_detail'),
+	
+	# Proposals
     url(r'^proposals/$', 'proposals'),
     url(r'^proposals/(?P<proposal_id>\d+)/$', 'proposal_detail'),
     url(r'^proposals/create/$', 'proposal_create'),
     url(r'^proposals/vote/(?P<proposal_id>\d+)/(?P<vote_direction>\S+)/$', 'proposal_vote'),
+    
+    # Login / logout
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'logout_user'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
--- parliament/views.py
+++ parliament/views.py
@@ -1,67 +1,92 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from parliament.models import Proposal, Tag, CreateProposalForm, ProposalVote, Person
 
+
 def index(request):
-	proposals_list = Proposal.objects.all().order_by('-timestamp')
-	return render(request, 'parliament/index.html', {'proposals_list': proposals_list})
+    """
+    If users are authenticated, direct them to the main page. Otherwise, take
+    them to the login page.
+    """
+    proposals_list = Proposal.objects.all().order_by('-timestamp')
+    return render(request, 'parliament/index.html', {'proposals_list': proposals_list})
+
 
 def proposals(request):
-	proposals_list = Proposal.objects.all().order_by('-timestamp')
-	return render(request, 'parliament/proposals.html', {'proposals_list': proposals_list})
+    proposals_list = Proposal.objects.all().order_by('-timestamp')
+    return render(request, 'parliament/proposals.html', {'proposals_list': proposals_list})
+
 
 def proposal_detail(request, proposal_id):
-	proposal = get_object_or_404(Proposal, pk=proposal_id)
-	return render(request, 'parliament/proposal_detail.html', {'proposal': proposal})
+    proposal = get_object_or_404(Proposal, pk=proposal_id)
+    return render(request, 'parliament/proposal_detail.html', {'proposal': proposal})
+
 
 def proposal_create(request):
-	if request.method == 'POST': # If the form has been submitted...
-		form = CreateProposalForm(request.POST) # A form bound to the POST data
-		if form.is_valid():
-			in_author = 'someone anonymous' # TODO get_or_404(person)
-			in_title = form.cleaned_data['title']
-			in_problem = form.cleaned_data['problem']
-			in_solution = form.cleaned_data['solution']
-			in_benefits = form.cleaned_data['benefits']
-			in_tags = form.cleaned_data['tags']
-			new_prop = Proposal.objects.create_proposal(in_author, in_title, in_problem, in_solution, in_benefits, in_tags)
-			return HttpResponseRedirect('/parliament/proposals/' + str(new_prop.id_num)) # Redirect after POST
-		else:
-			return HttpResponseRedirect('/parliament/proposal_create' + str(new_prop.id_num)) # Redirect after POST
-	else:
-		form = CreateProposalForm(auto_id=True) # An unbound form
-		return render(request, 'parliament/proposal_create.html', { 'form': form })
+    if request.method == 'POST':  # If the form has been submitted...
+        form = CreateProposalForm(request.POST)  # A form bound to the POST data
+        if form.is_valid():
+            in_author = 'someone anonymous'  # TODO get_or_404(person)
+            in_title = form.cleaned_data['title']
+            in_problem = form.cleaned_data['problem']
+            in_solution = form.cleaned_data['solution']
+            in_benefits = form.cleaned_data['benefits']
+            in_tags = form.cleaned_data['tags']
+            new_prop = Proposal.objects.create_proposal(in_author, in_title, in_problem, in_solution, in_benefits, in_tags)
+            return HttpResponseRedirect('/parliament/proposals/' + str(new_prop.id_num))  # Redirect after POST
+        else:
+            return HttpResponseRedirect('/parliament/proposal_create' + str(new_prop.id_num))  # Redirect after POST
+    else:
+        form = CreateProposalForm(auto_id=True)  # An unbound form
+        return render(request, 'parliament/proposal_create.html', {'form': form})
+
 
 def tags(request):
-	tags_list = Tag.objects.all().order_by('name')
-	return render(request, 'parliament/tags.html', {'tags_list': tags_list})
+    tags_list = Tag.objects.all().order_by('name')
+    return render(request, 'parliament/tags.html', {'tags_list': tags_list})
+
 
 def tag_detail(request, tag_name):
-	lowcase_name = tag_name.lower()
-	tag = get_object_or_404(Tag, pk=lowcase_name)
-	tagged_proposals = Proposal.objects.filter(tag__name = tag.name)
-	return render(request, 'parliament/tag_detail.html', {'tag': tag, 'tagged_proposals': tagged_proposals})
+    lowcase_name = tag_name.lower()
+    tag = get_object_or_404(Tag, pk=lowcase_name)
+    tagged_proposals = Proposal.objects.filter(tag__name=tag.name)
+    return render(request, 'parliament/tag_detail.html', {'tag': tag, 'tagged_proposals': tagged_proposals})
+
+
+# ========== Login ==========
+
+def logout_page(request):
+    """
+    Log users out and re-direct them to the main page.
+    """
+    logout(request)
+    return HttpResponseRedirect('parliament/')
+
+
+
 
 # ========== Actions ==========
 
+
 def proposal_vote(request, proposal_id, vote_direction):
-	proposal = get_object_or_404(Proposal, pk=proposal_id)	
-	voter = Person.objects.get(id_num = 5555) # TODO use login module
-	first_time_voting = (ProposalVote.objects.filter(who = voter, what = proposal).count() == 0) 
-	if first_time_voting:
-		# cast the vote
-		is_upvote = True if vote_direction == "up" else False
-		new_vote = ProposalVote.objects.create(who=voter, what=proposal, direction=is_upvote)
-		new_vote.save()
-		# update the number of votes
-		new_number = ProposalVote.objects.filter(what = proposal, direction=is_upvote).count()
-		if is_upvote:
-			proposal.upvotes = new_number
-		else:
-			proposal.downvotes = new_number
-		# end voting
-		proposal.save()
-		message = 'Voted successfully.'
-	else:
-		message = "You can't vote twice on the same proposal."
-	return render(request, 'parliament/proposal_detail.html', {'proposal': proposal, 'message': message})
+    proposal = get_object_or_404(Proposal, pk=proposal_id)
+    voter = Person.objects.get(id_num=5555)  # TODO use login module
+    first_time_voting = (ProposalVote.objects.filter(who=voter, what=proposal).count() == 0)
+    if first_time_voting:
+        # cast the vote
+        is_upvote = True if vote_direction == "up" else False
+        new_vote = ProposalVote.objects.create(who=voter, what=proposal, direction=is_upvote)
+        new_vote.save()
+        # update the number of votes
+        new_number = ProposalVote.objects.filter(what=proposal, direction=is_upvote).count()
+        if is_upvote:
+            proposal.upvotes = new_number
+        else:
+            proposal.downvotes = new_number
+        # end voting
+        proposal.save()
+        message = 'Voted successfully.'
+    else:
+        message = "You can't vote twice on the same proposal."
+    return render(request, 'parliament/proposal_detail.html', {'proposal': proposal, 'message': message})
